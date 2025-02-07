ddc9a769df81c3b01aa9c5549e08a93ee68e9c9b
Freakx198 <42691447+Freakx198@users.noreply.github.com>
2025-02-07 10:37:26 +0500

    Update practicecode01.js

--- JavaScript/practicecode01.js
+++ JavaScript/practicecode01.js
@@ -1,1 +1,11 @@
+console.log("me coding!")
 
+
+let num1 = parseFloat(prompt("enter the number"));
+let operator = prompt("enter the operator: +,-,*,/");
+let num2 = parseFloat(prompt("enter the number"));
+if (operator === "+") console.log(num1 + num2);
+else if (operator === "-") console.log(num1 - num2);
+else if (operator === "*") console.log(num1 * num2);
+else if (operator === "/") console.log(num1 / num2);
+else console.log("invalid operator");
